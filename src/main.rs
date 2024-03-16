9f5ea9736e70feb93ea9623d841e8a435e84fdee
Timothy Julian <timothyace73@gmail.com>
2024-03-17 00:00:56 +0700

    disect request

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use std::io::BufRead;
 use std::io::BufReader;
-use std::str;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::str;
 use std::thread;
 use std::time::Duration;
 
@@ -18,7 +19,6 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-
     let mut reader = BufReader::new(&mut stream);
     let buffer: Vec<u8> = match reader.fill_buf() {
         Ok(buf) => buf.to_vec(),
@@ -51,4 +51,32 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     println!("Request:\n{}", string);
+
+    let method = match req.method.ok_or("Method not found") {
+        Ok(method) => method,
+        Err(_) => {
+            return;
+        }
+    };
+    let uri = req.path.ok_or("URI not found").unwrap().to_string();
+    let version = req.version.ok_or("Version not found").unwrap().to_string();
+
+    let mut headers_map = HashMap::new();
+    for header in req.headers.iter() {
+        let name = header.name.to_string();
+        let value = std::str::from_utf8(header.value).unwrap().to_string();
+        headers_map.insert(name, value);
+    }
+
+    let body = if res < buffer.len() {
+        Some(String::from_utf8(buffer[res..].to_vec()))
+    } else {
+        None
+    };
+
+
+    println!("{}\n{}\n{}\n{:#?}\n{}", method, uri, version, headers_map, body.unwrap().unwrap());
+
+
+
 }
