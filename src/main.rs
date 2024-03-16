831e582f4a804821cc7437a1b471ed2b12db9d48
Timothy Julian <timothyace73@gmail.com>
2024-03-16 23:54:26 +0700

    add dependency httparse

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::BufRead;
 use std::io::BufReader;
+use std::str;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::thread;
@@ -17,12 +18,37 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        // should be handled gracefully
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("Request {:#?}", http_request);
+
+    let mut reader = BufReader::new(&mut stream);
+    let buffer: Vec<u8> = match reader.fill_buf() {
+        Ok(buf) => buf.to_vec(),
+        Err(err) => {
+            println!("Error reading buffer: {}", err);
+            return;
+        }
+    };
+
+    let mut headers = [httparse::EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+
+    println!("{:?}", buffer);
+
+    let res = match req.parse(&buffer).unwrap() {
+        httparse::Status::Complete(amt) => amt,
+        httparse::Status::Partial => {
+            return;
+        }
+    };
+
+    println!("{:?}", res);
+
+    let string = match str::from_utf8(&buffer) {
+        Ok(string) => string,
+        Err(e) => {
+            println!("Error reading UTF-8 sequence: {}", e);
+            return;
+        }
+    };
+
+    println!("Request:\n{}", string);
 }
